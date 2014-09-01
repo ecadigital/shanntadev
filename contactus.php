04b2ffb23e464bb432b9251fbf2588128bd128dc
GreanMaster <Mastergrean@gmail.com>
2014-09-01 18:00:51 +0700

    contactus

--- contactus.php
+++ contactus.php
@@ -21,14 +21,32 @@
                 <hr>
             </header>
             <div class="small-6 columns">
-                <h2>SHANNTA JEWELRY</h2>
+                <!-- <h2>SHANNTA JEWELRY</h2>
                 <p>
                     <b>THE UNIQUE ART PRODUCT WITH SEVERAL PRODUCT AND SERVICE ROOM</b>
                     306, 3rd Floor Bangkok Art and Cultural Center - Pathumwan Bangkok, Thailand 12390
                 </p>
                 <p>
                     <b>SHOP OPEN</b> : Tru - Sun. : 10 am - 9 pm. <b>TELLEPHONE</b> : +(66)2-214-3018
-                </p>
+                </p> -->
+                <h1>H1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, ex.</h1>
+                <h2>H2 Lorem ipsum dolor sit amet.</h2>
+                <h3>H3 Lorem ipsum dolor sit amet.</h3>
+                <h4>H4 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt repellat, ex nesciunt repellendus! Sit ratione harum libero, odio perferendis autem nostrum quaerat quos excepturi dolorem! Ex adipisci cumque temporibus quasi.</h4>
+                <h5>H5 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo exercitationem ducimus aut dolorem, incidunt earum commodi facere alias aliquid! Nostrum quam eligendi beatae quo amet magnam illo veritatis quos placeat?</h5>
+                <h6>h6 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus repudiandae necessitatibus veritatis eaque fuga, magnam, nisi. Officiis voluptatem a architecto ipsam ea, debitis provident hic aliquam ipsum! Autem, cumque, voluptatibus.</h6>
+                <ul>
+                    <li>li Lorem ipsum.</li>
+                    <ul>
+                        <li>ul>li Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam alias aspernatur quisquam nemo ducimus, aliquam totam rerum neque asperiores similique ad vel reprehenderit commodi praesentium atque nostrum assumenda mollitia. Aliquam.</li>
+                    </ul>
+                </ul>
+                <p>p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, aliquam.</p>
+                <p>p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, aliquam.</p>
+                <p class="x04">p.x04 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque dolor porro. Reprehenderit, officiis eius laboriosam at autem. Aliquid eveniet nulla perferendis nam natus cumque dolore laudantium earum at, dignissimos?</p>
+                <p class="x02">p.x02 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque dolor porro. Reprehenderit, officiis eius laboriosam at autem. Aliquid eveniet nulla perferendis nam natus cumque dolore laudantium earum at, dignissimos?</p>
+                <p class="x2">p.x2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque dolor porro. Reprehenderit, officiis eius laboriosam at autem. Aliquid eveniet nulla perferendis nam natus cumque dolore laudantium earum at, dignissimos?</p>
+                <p class="x4">p.x4 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque dolor porro. Reprehenderit, officiis eius laboriosam at autem. Aliquid eveniet nulla perferendis nam natus cumque dolore laudantium earum at, dignissimos?</p>
             </div>
             <div class="small-6 columns"></div>
         </div><!-- #content .row.cart -->
