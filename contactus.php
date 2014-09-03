98fe0875cb39daed8a2a817dce6cc9bd938f81aa
GreanMaster <Mastergrean@gmail.com>
2014-09-03 17:28:08 +0700

    jhjhjhj
    kjkjkjkjkk

--- contactus.php
+++ contactus.php
@@ -21,15 +21,15 @@
                 </div>
             </header>
             <div class="small-12 large-7 columns end">
-                <h2>SHANNTA JEWELRY</h2>
+                <h3>SHANNTA JEWELRY</h3>
                 <p><b>THE UNIQUE ART PRODUCT WITH SEVERAL PRODUCT AND SERVICE ROOM</b><br>
                 306, 3rd Floor Bangkok Art and Cukturat Center - Pathumwan Bangkok, Thailand 12390</p> 
                 <p><b>SHOP OPEN</b> : Tue - Sun : 10 am - 9 om. <b>TELLEPHONE :</b> +(66)2-214-3018</p>
             </div>
-            <div class="small-12 large-7 columns">
+            <div class="small-12 medium-7 columns">
                 <img src="<?php echo __images__;?>/dummy-map.png" alt="">
             </div>
-            <div class="small-12 large-5 column">
+            <div class="small-12 medium-5 column">
                 <form>
                     <input type="text" placeholder="Name">
                     <input type="text" placeholder="Email">
@@ -40,6 +40,89 @@
                     </div>
                 </form>
             </div>
+            <div class="small-12 columns">
+                <hr>
+            </div>
+            <div class="small-12 columns">
+                <h2 style="color: #D8D2D6;">STORE LOCATION</h2>
+            </div>
+            <div class="row">
+                <div class="small-3 medium-1 columns">
+                    <div class="map-marker">
+                        <div>
+                            <img src="<?php echo __images__;?>/assets/marker.png" alt="">
+                            <span>A</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="small-9 medium-6 column">
+                    <h3>SHANNTA JEWELRY STORE A</h3>
+                    <p>
+                        Central Festival Pattaya43 miles1 Floor,33/98 Moo 9,Pattaya Beach Road,
+                        Banglamung, Pattaya Chonburi, 20260 Phone : +622-804-3371
+                    </p>
+                    <p>
+                        <b>INFOREMATIONSHOP :</b> <br>
+                        it all started nearly 30 years ago. Back in 1982 a jeweller's shop that would one day
+                        become by Danish goldsmith Per Enevold sen and his wife Winnie. <a href="#">View map</a>
+                    </p>
+                </div>
+                <div class="small-12 medium-5 column">
+                    <img src="<?php echo __images__;?>/dummy-map-2.png" alt="">
+                </div>
+            </div><!-- .row -->
+            <hr class="smallest">
+            <div class="row">
+                <div class="small-3 medium-1 columns">
+                    <div class="map-marker">
+                        <div>
+                            <img src="<?php echo __images__;?>/assets/marker.png" alt="">
+                            <span>B</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="small-9 medium-6 column">
+                    <h3>SHANNTA JEWELRY STORE B</h3>
+                    <p>
+                        Central Festival Pattaya43 miles1 Floor,33/98 Moo 9,Pattaya Beach Road,
+                        Banglamung, Pattaya Chonburi, 20260 Phone : +622-804-3371
+                    </p>
+                    <p>
+                        <b>INFOREMATIONSHOP :</b> <br>
+                        it all started nearly 30 years ago. Back in 1982 a jeweller's shop that would one day
+                        become by Danish goldsmith Per Enevold sen and his wife Winnie. <a href="#">View map</a>
+                    </p>
+                </div>
+                <div class="small-12 medium-5 column">
+                    <img src="<?php echo __images__;?>/dummy-map-3.png" alt="">
+                </div>
+            </div><!-- .row -->
+            <hr class="smallest">
+            <div class="row">
+                <div class="small-3 medium-1 columns">
+                    <div class="map-marker">
+                        <div>
+                            <img src="<?php echo __images__;?>/assets/marker.png" alt="">
+                            <span>C</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="small-9 medium-6 column">
+                    <h3>SHANNTA JEWELRY STORE C</h3>
+                    <p>
+                        Central Festival Pattaya43 miles1 Floor,33/98 Moo 9,Pattaya Beach Road,
+                        Banglamung, Pattaya Chonburi, 20260 Phone : +622-804-3371
+                    </p>
+                    <p>
+                        <b>INFOREMATIONSHOP :</b> <br>
+                        it all started nearly 30 years ago. Back in 1982 a jeweller's shop that would one day
+                        become by Danish goldsmith Per Enevold sen and his wife Winnie. <a href="#">View map</a>
+                    </p>
+                </div>
+                <div class="small-12 medium-5 column">
+                    <img src="<?php echo __images__;?>/dummy-map-4.png" alt="">
+                </div>
+            </div><!-- .row -->
         </div><!-- #content .row.cart -->
         <?php require "inc/layouts/footer-tag.php"; ?>
         <?php require "inc/layouts/javascript.php"; ?>
