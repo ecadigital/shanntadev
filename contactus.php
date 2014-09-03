528cf129e702998ff48bcda66bc086cd7f583d6e
GreanMaster <Mastergrean@gmail.com>
2014-09-03 10:58:20 +0700

    temp

--- contactus.php
+++ contactus.php
@@ -15,45 +15,31 @@
         <div id="content" class="row page">
             <header>
                 <h1>CONTACT US </h1>
-                <hr>
+                <!-- <hr> -->
                 <div class="headImage">
                     <img src="<?php echo __images__;?>/contactus-h.png" alt="">
                 </div>
             </header>
-            <div class="small-12 large-8 columns">
-                <!-- <h2>SHANNTA JEWELRY</h2>
-                <p>
-                    <b>THE UNIQUE ART PRODUCT WITH SEVERAL PRODUCT AND SERVICE ROOM</b>
-                    306, 3rd Floor Bangkok Art and Cultural Center - Pathumwan Bangkok, Thailand 12390
-                </p>
-                <p>
-                    <b>SHOP OPEN</b> : Tru - Sun. : 10 am - 9 pm. <b>TELLEPHONE</b> : +(66)2-214-3018
-                </p> -->
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia repellendus facere eveniet, architecto ipsa nostrum? Repudiandae voluptate vero assumenda, cumque vitae. Earum assumenda amet porro esse tenetur dolores quisquam maiores.
-                <h1>H1 Lorem ipsum dolor sit amet.</h1>
-                <h2>H2 Lorem ipsum dolor sit amet.</h2>
-                <h3>H3 Lorem ipsum dolor sit amet.</h3>
-                <h4>H4 Lorem ipsum dolor sit amet.</h4>
-                <h5>H5 Lorem ipsum dolor sit amet.</h5>
-                <h6>H6 Lorem ipsum dolor sit amet.</h6>
-                <ul>
-                    <li>li Lorem ipsum.</li>
-                    <ul>
-                        <li>ul>li Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam alias aspernatur quisquam nemo ducimus, aliquam totam rerum neque asperiores similique ad vel reprehenderit commodi praesentium atque nostrum assumenda mollitia. Aliquam.</li>
-                    </ul>
-                </ul>
-                <p>p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, aliquam.</p>
-                <p>p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, aliquam.</p>
-                <b>b Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium dignissimos nulla dolor vero fugit quasi officiis temporibus nobis, excepturi iusto, dolore. Distinctio rem eius tenetur. Iure ipsam nulla, aliquam perferendis!</b>
-                <u>u Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam fuga inventore eos ut laborum numquam ullam quod debitis tempora nostrum, sequi atque eaque, nulla quasi repellendus magnam aut enim. Soluta.</u>
-                <i>i Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur omnis, consequuntur maxime id, ipsam saepe atque rem culpa rerum doloribus dolorem perspiciatis distinctio sequi. Fugiat quo illum, sit eligendi quas!</i>
-                <p class="x04">p.x04 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque dolor porro. Reprehenderit, officiis eius laboriosam at autem. Aliquid eveniet nulla perferendis nam natus cumque dolore laudantium earum at, dignissimos?</p>
-                <p class="x02">p.x02 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque dolor porro. Reprehenderit, officiis eius laboriosam at autem. Aliquid eveniet nulla perferendis nam natus cumque dolore laudantium earum at, dignissimos?</p>
-                <p class="x2">p.x2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque dolor porro. Reprehenderit, officiis eius laboriosam at autem. Aliquid eveniet nulla perferendis nam natus cumque dolore laudantium earum at, dignissimos?</p>
-                <p class="x25">p.x25 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque dolor porro. Reprehenderit, officiis eius laboriosam at autem. Aliquid eveniet nulla perferendis nam natus cumque dolore laudantium earum at, dignissimos?</p>
-                <p class="x4">p.x4 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla cumque dolor porro. Reprehenderit, officiis eius laboriosam at autem. Aliquid eveniet nulla perferendis nam natus cumque dolore laudantium earum at, dignissimos?</p>
+            <div class="small-12 large-7 columns end">
+                <h2>SHANNTA JEWELRY</h2>
+                <p><b>THE UNIQUE ART PRODUCT WITH SEVERAL PRODUCT AND SERVICE ROOM</b><br>
+                306, 3rd Floor Bangkok Art and Cukturat Center - Pathumwan Bangkok, Thailand 12390</p> 
+                <p><b>SHOP OPEN</b> : Tue - Sun : 10 am - 9 om. <b>TELLEPHONE :</b> +(66)2-214-3018</p>
             </div>
-            <div class="small-6 columns"></div>
+            <div class="small-12 large-7 columns">
+                <img src="<?php echo __images__;?>/dummy-map.png" alt="">
+            </div>
+            <div class="small-12 large-5 column">
+                <form>
+                    <input type="text" placeholder="Name">
+                    <input type="text" placeholder="Email">
+                    <input type="text" placeholder="Tel">
+                    <textarea rows="5">Massage</textarea>
+                    <div class="ta-right">
+                        <input type="submit" value="Send Massage >" class="button">
+                    </div>
+                </form>
+            </div>
         </div><!-- #content .row.cart -->
         <?php require "inc/layouts/footer-tag.php"; ?>
         <?php require "inc/layouts/javascript.php"; ?>
