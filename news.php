55ced29d24517c0c7c347ba44fc4df1d1e07730b
tuckkiez <i3adtzmaru@gmail.com>
2014-09-05 15:59:38 +0700

    news

--- news.php
+++ news.php
@@ -16,6 +16,54 @@
             <header>
                 <h1>NEWS &amp; EVENT</h1>
             </header>
+            <div id="news1"><img src="img/demo/news1.jpg"></div>
+            <h1 style="font-weight:bold;color:#d8d2d5">25.04.2014</h1>
+            <div id="clearfix"></div>
+            <h2>The PANDORA story is about a company with a distinctive brand and distinctive products A that in just 
+            years has<br> made exceptional.</h2>
+            <p>It all started nearly 30 years ago. Back in 1982 a jeweller’s shop that would
+            one day become PANDORA was established in modest surroundings in
+            Copenhagen, Denmark, by Danish goldsmith Per Enevoldsen and his wife 
+            Winnie. From the beginning, they often travelled to Thailand in search of
+            jewelry for importing. As the demand for their products increased, their focus
+            gradually shifted towards wholesale to clients in Denmark.
+            <br><br>
+
+            In 1987, after several successful years as wholesalers, the retail activities
+            were discontinued and the company moved to larger premises. Meanwhile,
+            the first in-house designer joined the company and PANDORA began focus
+            on creating its own unique jewelry. In 1989, the company decided to start
+            manufacturing its jewelry in Thailand.
+            <br><br>
+            Over the following years, PANDORA rapidly expanded its market presence
+            from a Scandinavian platform to an international marketing and sales plat
+            form driven mainly by third party distributors and strong production capabi
+            lities in Thailand.
+            <br><br>
+            In order to increase its production capacity, PANDORA opened a large scale,
+            six-story fully-owned manufacturing facility in Thailand in 2005, which still 
+            orms a central part of the current production and infrastructure. In August
+            2008 a second manufacturing facility was opened in the same area, which
+            in 2010 was followed by the opening of PANDORA’s third and fourth facilities
+            in the same area, further strengthening our unique production setup.
+            </p>
+            <hr>
+            <div id="news-nav">
+                <h2>NEWS & EVENT MORE</h2><a href="#">BACK></a>
+                
+            </div>
+            
+            <div id="news2"><img src="img/demo/news2.jpg"></div>
+            <div id="p-news"><h1 style="font-weight:bold;color:#d8d2d5">25.04.2014</h1>
+            <h2>The PANDORA story is about a company with a
+                distinctive brand and distinctive products A
+                that in just  years has made exceptional.</h2>
+                <p>It all started nearly 30 years ago. Back in 1982 a jeweller’s shop that
+would one day become PANDORA was established in modest surro
+undings in Copenhagen, Denmark, by Danish goldsmith Per Enevold
+sen and his wife Winnie.</p>
+            </div>
+
         </div><!-- #content .row.cart -->
         <?php require "inc/layouts/footer-tag.php"; ?>
         <?php require "inc/layouts/javascript.php"; ?>
